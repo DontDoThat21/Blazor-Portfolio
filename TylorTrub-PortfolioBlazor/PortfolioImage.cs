60061c191901709510c8d6b68d7661f37a574fbf
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2025-08-31 05:12:18 -0400

    Add user roles, claims, and entity classes
    This commit introduces several new classes related to user roles and claims, including `AspNetRole`, `AspNetRoleClaim`, and `AspNetUser`, among others. Each class is defined in the `TylorTrubPortfolioBlazor.UI` namespace with properties initialized to support non-nullable reference types.
    
    The `PortfolioContext` class is added, extending `DbContext` to manage database operations effectively. It includes `DbSet` properties for each entity and overrides the `OnModelCreating` method to establish relationships and constraints, enhancing the overall data model.
    
    These changes improve the management of user roles, products, orders, and other entities crucial for the portfolio management system's functionality.

---
+++ TylorTrub-PortfolioBlazor/PortfolioImage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TylorTrubPortfolioBlazor.UI;
+
+public partial class PortfolioImage
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string FileName { get; set; } = null!;
+
+    public string DisplayText { get; set; } = null!;
+}
---
+++ TylorTrub-PortfolioBlazor/Project.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TylorTrubPortfolioBlazor.UI;
+
+public partial class Project
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Url { get; set; } = null!;
+
+    public string ProjectPhoto { get; set; } = null!;
+
+    public string Language { get; set; } = null!;
+}
